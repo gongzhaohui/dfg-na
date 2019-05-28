79d554067ee45350a00430ec35743b85c89c40cd
gongzhaohui <gongzhaohui@gmail.com>
2019-05-28 10:53:09 +0800

    ignore database-pdr

--- server/src/entities/history.entity.ts
+++ server/src/entities/history.entity.ts
@@ -1,21 +1,31 @@
 import { Entity, Column,  PrimaryColumn } from 'typeorm';
 import { SupperEntity } from '../base';
 
-@Entity('dfg_tyuzan')
+@Entity('dfg_na_history')
 export class History extends SupperEntity  {
-  @PrimaryColumn({type: 'int', name: 'cdefine34'})
-  id: number;
+  @PrimaryColumn({type: 'int', name: 'isodid'})
+  isodid: number;
 
-@Column({type: 'nvarchar', length: 14, name: 'cdefine31'})
+@Column({type: 'nvarchar', length: 14, name: 'jno'})
 jno: string;
+@Column({type: 'nvarchar', length: 14, name: 'mno'})
+mno: string;
 @Column({type: 'nvarchar', length: 60, name: 'ccuscode'})
 cuscode: string;
 @Column({type: 'nvarchar', length: 255, name: 'ccusabbname'})
 cusname: string;
+@Column({type: 'nvarchar', length: 60, name: 'cinvcode'})
+cinvcode: string;
 @Column({type: 'nvarchar', length: 60, name: 'cinvaddcode'})
 cinvaddcode: string;
 @Column({type: 'nvarchar', length: 60, name: 'cinvstd'})
 cinvstd: string;
 @Column({type: 'int',  name: 'iquantity'})
 qty: number;
+@Column({type: 'smalldatetime',  name: 'ddate'})
+ddate: number;
+@Column({type: 'smalldatetime',  name: 'mdate'})
+mdate: number;
+@Column({type: 'int',  name: 'moid'})
+moid: number;
 }
