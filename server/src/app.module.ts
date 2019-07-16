b9eb2b8d8524daa3495712a706a1d3707dd48231
gongzhaohui <gongzhaohui@gmail.com>
2019-07-16 13:16:29 +0800

    typeorm issues error when production mode yet

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import {DatabaseModule} from './database/database.module';
-import { SoModule } from './modules/so/so.module';
-// import {OpenSoController} from './entities/openso.controller';
-import { SearchModule } from './modules/search/search.module';
 import { InventoryModule } from './modules/inventory/inventory.module';
 import { DatabaseModule } from './modules/database/database.module';
 import { HistoryModule } from './modules/history/history.module';
@@ -13,15 +9,13 @@ import { HourRateModule } from './modules/hourrate/hourrate.module';
 @Module({
   imports: [
     DatabaseModule,
-    SoModule,
-    SearchModule,
     InventoryModule,
     BomPlanModule,
     HourRateModule,
     HistoryModule,
   ],
   controllers: [
-    /*AppController*/
+    AppController,
   ],
   providers: [AppService],
 })
